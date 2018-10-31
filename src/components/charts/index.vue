d896f79a5312374b0b4806a44d97c16f9658e160
Andrey Reinwald <andreinwald@gmail.com>
2018-10-31 14:41:25 +0200

    Moved styles to styles.css

--- src/components/charts/index.vue
+++ src/components/charts/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="charts_wrapper">
         <div class="metric_title">
             <div v-on:click="goBack()" class="back"></div>
             <div class="item_title">{{metric}}</div>
@@ -76,65 +76,3 @@
     }
   }
 </script>
-
-<style scoped>
-    .metric_title {
-        width: 100%;
-        height: 50px;
-        line-height: 50px;
-        background: #fff;
-        position: relative;
-    }
-
-    .toolbar {
-        display: flex;
-        justify-content: space-around;
-        height: 50px;
-        align-content: center;
-        align-items: center;
-        box-sizing: border-box;
-        background: rgba(69, 140, 201, 1);
-        border-bottom: 2px solid rgba(69, 140, 201, 1);
-    }
-
-    .toolbar > div {
-        text-transform: uppercase;
-        font-size: 14px;
-        padding: 5px 15px 5px 15px;
-        color: #fff;
-    }
-
-    .toolbar > .active {
-        border: 2px solid #fff;
-    }
-
-    .back {
-        position: absolute;
-        width: 0;
-        height: 0;
-        border-style: solid;
-        border-width: 12.5px 21.7px 12.5px 0;
-        border-color: transparent #458cc9 transparent transparent;
-        top: 12px;
-        left: 20px;
-    }
-
-    .item_count {
-        position: absolute;
-        right: 15px;
-        height: 30px;
-        background: #efefef;
-        padding-left: 15px;
-        padding-right: 15px;
-        line-height: 30px;
-        top: 10px;
-    }
-
-    .item_title {
-        width: 45%;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        margin-left: 70px;
-    }
-
-</style>
